65b629956ddbaad4792a8bdab72324cc1d9cd623
Karl Martin Teras <karl.martin@ter.as>
2022-12-02 07:13:30 +0200

    Add solution to day 2

---
+++ lib/day02p1.ex
@@ -0,0 +1,33 @@
+defmodule Day02P1 do
+
+  def solve(filename) do
+    File.read!(filename)
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&score/1)
+    |> Enum.sum()
+  end
+
+  defp score(list) do
+    [o, m] = list
+    |> String.split(" ")
+
+    case m do
+      "X" -> case o do
+        "A" -> 3 + 1
+        "B" -> 0 + 1
+        "C" -> 6 + 1
+      end
+      "Y" -> case o do
+       "A" -> 6 + 2
+       "B" -> 3 + 2
+       "C" -> 0 + 2
+      end
+      "Z" -> case o do
+       "A" -> 0 + 3
+       "B" -> 6 + 3
+       "C" -> 3 + 3
+     end
+    end
+  end
+end
